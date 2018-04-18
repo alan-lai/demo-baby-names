fcf1eb2b2b56c527dd02642f0ad0fe81024f4aa9
Alan Lai <alan.fl.lai@gmail.com>
2018-04-18 13:19:18 +1000

    added hr when shortlist has items

--- src/components/ShortList.js
+++ src/components/ShortList.js
@@ -10,10 +10,11 @@ export default ({ data, favourites, toggleFavourite }) => {
     });
     return (
         <div className="favourites">
-            <h4>Click on a name to shortlist it...</h4>
+            <h4>{ favourites.length ? 'Your Shortlist' : 'Click on a name to shortlist it...' }</h4>
             <ul>
                 { namesList }
             </ul>
+            { favourites.length ? <hr /> : '' }
         </div>
     )
 }
